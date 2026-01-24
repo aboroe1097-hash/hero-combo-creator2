ee3e36b00bb954e57c54c09437bdec2b9abb970b
aboroe1097-hash <aboroe1097@gmail.com>
2026-01-24 19:46:06 +0400

    Update combos-db.js

--- js/combos-db.js
+++ js/combos-db.js
@@ -1,8 +1,61 @@
 // js/combos-db.js
 // Simple ranked combos DB: each entry = 3 heroes + score.
-// Add / edit combos here as the meta changes.
+// Mapping order: [Front, Middle, Back]
+// Data extracted from VTS 1097 "Last Best Combos" 
 
 export const rankedCombos = [
+  // --- High Tier Meta (Top Performers) ---
+  {
+    heroes: ["Beast Queen", "Rozen Blade", "Immortal"],
+    score: 99
+  },
+  {
+    heroes: ["Octavius", "Cleopatra VII", "Caesar"],
+    score: 98
+  },
+  {
+    heroes: ["Sky Breaker", "Ramses II", "Al Fatih"],
+    score: 97
+  },
+  {
+    heroes: ["Charles Great", "Inquisitor", "Witch Hunter"],
+    score: 96
+  },
+
+  {
+    heroes: ["War Lord", "Black Prince", "Jane"],
+    score: 94
+  },
+  {
+    heroes: ["War Lord", "Constantine", "Jane"],
+    score: 93
+  },
+  {
+    heroes: ["War Lord", "Black Prince", "Lionheart"],
+    score: 92
+  },
+  {
+    heroes: ["Black Prince", "Jeanne d'Arc", "Lionheart"],
+    score: 90
+  },
+
+  {
+    heroes: ["Edward", "Charles Great", "Al Fatih"],
+    score: 89
+  },
+  {
+    heroes: ["War Lord", "Isabella I", "Mary Tudor"],
+    score: 88
+  },
+  {
+    heroes: ["Peace Bringer", "Genghis Khan", "The Heroine"],
+    score: 87
+  },
+  {
+    heroes: ["Demon Spear", "Jiguang Qi", "North's Rage"],
+    score: 85
+  },
+
   {
     heroes: ["Jeanne d'Arc", "Leonidas", "The Heroine"],
     score: 100
@@ -10,19 +63,8 @@ export const rankedCombos = [
   {
     heroes: ["Genghis Khan", "Al Fatih", "Lionheart"],
     score: 97
-  },
-  {
-    heroes: ["Yukimura Sanada", "Jade", "Immortal Guardian"],
-    score: 95
-  },
-  {
-    heroes: ["William the Conqueror", "Cao Cao", "Alfred"],
-    score: 94
-  },
-  {
-    heroes: ["Ramses II", "Octavius", "Immortal"],
-    score: 93
   }
-  // 👉 Add more like:
+    // 👉 Add more like:
   // { heroes: ["Hero A", "Hero B", "Hero C"], score: 90 },
 ];
+
