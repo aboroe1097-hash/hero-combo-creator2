522644ece8f259b0ae90d73092e167808c9f743e
aboroe1097-hash <aboroe1097@gmail.com>
2026-01-25 03:12:34 +0400

    Update combos-db.js

--- js/combos-db.js
+++ js/combos-db.js
@@ -1,143 +1,43 @@
 // js/combos-db.js
-// Ranked combos database: 3 heroes [Front, Middle, Back] + score.
-// Corrected typos and names for 11-language compatibility.
+// Rank is determined by position in this array. First item = Rank 1.
 
 export const rankedCombos = [
-  // --- TOP META / GARRISON TIER 1 (Scores 95-100) ---
-  {
-    heroes: ["Theodora", "Ramses II", "Jade Eagle"],
-    score: 100
-  },
-  {
-    heroes: ["Bleeding Steed", "Ramses II", "Jade Eagle"],
-    score: 99
-  },
-  {
-    heroes: ["Charles the Great", "Ramses II", "Al Fatih"],
-    score: 98
-  },
-  {
-    heroes: ["Theodora", "The Brave", "Jade Eagle"],
-    score: 97
-  },
-  {
-    heroes: ["Octavius", "Cleopatrra VII", "Ceasar"],
-    score: 96
-  },
-  {
-    heroes: ["King Arthur", "Bleeding Steed", "Cleopatrra VII"],
-    score: 95
-  },
+  // --- TIER 1 (TOP META / GARRISON) ---
+  { heroes: ["Theodora", "Ramses II", "Jade Eagle"] },
+  { heroes: ["Bleeding Steed", "Ramses II", "Jade Eagle"] },
+  { heroes: ["Charles the Great", "Ramses II", "Al Fatih"] },
+  { heroes: ["Theodora", "The Brave", "Jade Eagle"] },
+  { heroes: ["Octavius", "Cleopatrra VII", "Ceasar"] },
+  { heroes: ["King Arthur", "Bleeding Steed", "Cleopatrra VII"] },
 
-  // --- STRONG META / GARRISON TIER 2 (Scores 85-94) ---
-  {
-    heroes: ["Octavius", "The Brave", "Ceasar"],
-    score: 94
-  },
-  {
-    heroes: ["Alfred", "Bleeding Steed", "Cleopatrra VII"],
-    score: 93
-  },
-  {
-    heroes: ["King Arthur", "Bleeding Steed", "Cao Cao"],
-    score: 92
-  },
-  {
-    heroes: ["Sky Breaker", "Ramses II", "Al Fatih"],
-    score: 91
-  },
-  {
-    heroes: ["The Brave", "Rozen Blade", "Immortal"],
-    score: 90
-  },
-  {
-    heroes: ["The Brave", "Bleeding Steed", "Jade Eagle"],
-    score: 89
-  },
-  {
-    heroes: ["The Brave", "BeastQueen", "Immortal"],
-    score: 88
-  },
-  {
-    heroes: ["Immortal Guardian", "Bleeding Steed", "Jade Eagle"],
-    score: 87
-  },
-  {
-    heroes: ["Octavius", "BeastQueen", "Immortal"],
-    score: 86
-  },
-  {
-    heroes: ["Alfred", "Black Prince", "Jeanne d'Arc"],
-    score: 85
-  },
+  // --- TIER 2 (STRONG META) ---
+  { heroes: ["Octavius", "The Brave", "Ceasar"] },
+  { heroes: ["Alfred", "Bleeding Steed", "Cleopatrra VII"] },
+  { heroes: ["King Arthur", "Bleeding Steed", "Cao Cao"] },
+  { heroes: ["Sky Breaker", "Ramses II", "Al Fatih"] },
+  { heroes: ["The Brave", "Rozen Blade", "Immortal"] },
+  { heroes: ["The Brave", "Bleeding Steed", "Jade Eagle"] },
+  { heroes: ["The Brave", "BeastQueen", "Immortal"] },
+  { heroes: ["Immortal Guardian", "Bleeding Steed", "Jade Eagle"] },
+  { heroes: ["Octavius", "BeastQueen", "Immortal"] },
+  { heroes: ["Alfred", "Black Prince", "Jeanne d'Arc"] },
 
-  // --- CLASSIC / SEASONAL SYNERGIES (Scores 70-84) ---
-  {
-    heroes: ["Charles the Great", "Edward the Confessor", "Al Fatih"],
-    score: 84
-  },
-  {
-    heroes: ["Sky Breaker", "Charles the Great", "Inquisitor"],
-    score: 83
-  },
-  {
-    heroes: ["War Lord", "Jeanne d'Arc", "Mary Tudor"],
-    score: 82
-  },
-  {
-    heroes: ["Alfred", "Jeanne d'Arc", "Mary Tudor"],
-    score: 81
-  },
-  {
-    heroes: ["Charles the Great", "Inquisitor", "Witch Hunter"],
-    score: 80
-  },
-  {
-    heroes: ["Black Prince", "Jeanne d'Arc", "Lionheart"],
-    score: 79
-  },
-  {
-    heroes: ["War Lord", "Isabella I", "Mary Tudor"],
-    score: 78
-  },
-  {
-    heroes: ["Black Prince", "Isabella I", "Lionheart"],
-    score: 77
-  },
-  {
-    heroes: ["War Lord", "Black Prince", "Jane"],
-    score: 76
-  },
-  {
-    heroes: ["War Lord", "Rokuboshuten", "Jane"],
-    score: 75
-  },
-  {
-    heroes: ["War Lord", "Constantine the Great", "Jane"],
-    score: 74
-  },
-  {
-    heroes: ["Edward the Confessor", "Leonidas", "Al Fatih"],
-    score: 73
-  },
-  {
-    heroes: ["Edward the Confessor", "Isabella I", "Al Fatih"],
-    score: 72
-  },
-  {
-    heroes: ["Peace Bringer", "Genghis Khan", "The Heroine"],
-    score: 71
-  },
-  {
-    heroes: ["Demon Spear", "Jiguang Qi", "Genghis Khan"],
-    score: 70
-  },
-  {
-    heroes: ["Demon Spear", "Jiguang Qi", "North's Rage"],
-    score: 69
-  },
-  {
-    heroes: ["Demon Spear", "Queen Anne", "William Wallace"],
-    score: 68
-  }
+  // --- TIER 3 (SYNERGY) ---
+  { heroes: ["Charles the Great", "Edward the Confessor", "Al Fatih"] },
+  { heroes: ["Sky Breaker", "Charles the Great", "Inquisitor"] },
+  { heroes: ["War Lord", "Jeanne d'Arc", "Mary Tudor"] },
+  { heroes: ["Alfred", "Jeanne d'Arc", "Mary Tudor"] },
+  { heroes: ["Charles the Great", "Inquisitor", "Witch Hunter"] },
+  { heroes: ["Black Prince", "Jeanne d'Arc", "Lionheart"] },
+  { heroes: ["War Lord", "Isabella I", "Mary Tudor"] },
+  { heroes: ["Black Prince", "Isabella I", "Lionheart"] },
+  { heroes: ["War Lord", "Black Prince", "Jane"] },
+  { heroes: ["War Lord", "Rokuboshuten", "Jane"] },
+  { heroes: ["War Lord", "Constantine the Great", "Jane"] },
+  { heroes: ["Edward the Confessor", "Leonidas", "Al Fatih"] },
+  { heroes: ["Edward the Confessor", "Isabella I", "Al Fatih"] },
+  { heroes: ["Peace Bringer", "Genghis Khan", "The Heroine"] },
+  { heroes: ["Demon Spear", "Jiguang Qi", "Genghis Khan"] },
+  { heroes: ["Demon Spear", "Jiguang Qi", "North's Rage"] },
+  { heroes: ["Demon Spear", "Queen Anne", "William Wallace"] }
 ];
