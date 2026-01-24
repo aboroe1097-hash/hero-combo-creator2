5f89a5b760250248ab55cc1355351d144bff4514
aboroe1097-hash <aboroe1097@gmail.com>
2026-01-25 03:16:29 +0400

    Update combos-db.js

--- js/combos-db.js
+++ js/combos-db.js
@@ -1,6 +1,27 @@
 // js/combos-db.js
 // Rank is determined by position in this array. First item = Rank 1.
 
+// --- Season 0 ---
+// "Jeanne d'Arc", "Isabella I", "Jiguang Qi", "Mary Tudor", "Leonidas", 
+// "The Boneless", "Demon Spear", "Kublai", "The Heroine", "Queen Anne", 
+// "North's Rage", "William Wallace", "Yukimura Sanada", "Heaven's Justice"
+
+// --- Season 1 ---
+// "Alfred", "Cao Cao", "Charles the Great", "Black Prince", "Lionheart", 
+// "Al Fatih", "Edward the Confessor", "Constantine the Great", 
+// "Genghis Khan", "William the Conqueror"
+
+// --- Season 2 ---
+// "Inquisitor", "BeastQueen", "Jade", "Immortal", "Peace Bringer", 
+// "Witch Hunter", "Ramses II", "Octavius"
+
+// --- Season 3 ---
+// "War Lord", "Jane", "Sky Breaker", "Rokuboshuten", "Bleeding Steed", 
+// "Rozen Blade", "Celopatrra VII", "Ceasar"
+
+// --- Season 4 ---
+// "Desert Storm", "Soaring Hawk", "The Brave", "Jade Eagle", 
+// "Immortal Guardian", "Divine Arrow", "Theodora", "King Arthur"
 export const rankedCombos = [
   // --- TIER 1 (TOP META / GARRISON) ---
   { heroes: ["Theodora", "Ramses II", "Jade Eagle"] },
@@ -12,12 +33,15 @@ export const rankedCombos = [
 
   // --- TIER 2 (STRONG META) ---
   { heroes: ["Octavius", "The Brave", "Ceasar"] },
-  { heroes: ["Alfred", "Bleeding Steed", "Cleopatrra VII"] },
   { heroes: ["King Arthur", "Bleeding Steed", "Cao Cao"] },
   { heroes: ["Sky Breaker", "Ramses II", "Al Fatih"] },
+  { heroes: ["Alfred", "Bleeding Steed", "Cleopatrra VII"] },
   { heroes: ["The Brave", "Rozen Blade", "Immortal"] },
   { heroes: ["The Brave", "Bleeding Steed", "Jade Eagle"] },
-  { heroes: ["The Brave", "BeastQueen", "Immortal"] },
+  { heroes: ["BeastQueen", "Rozen Blade", "Immortal"] },
+  
+  { heroes: ["Peace Bringer", "Bleeding Steed", "Jade"] },
+  
   { heroes: ["Immortal Guardian", "Bleeding Steed", "Jade Eagle"] },
   { heroes: ["Octavius", "BeastQueen", "Immortal"] },
   { heroes: ["Alfred", "Black Prince", "Jeanne d'Arc"] },
@@ -37,6 +61,10 @@ export const rankedCombos = [
   { heroes: ["Edward the Confessor", "Leonidas", "Al Fatih"] },
   { heroes: ["Edward the Confessor", "Isabella I", "Al Fatih"] },
   { heroes: ["Peace Bringer", "Genghis Khan", "The Heroine"] },
+  
+  { heroes: ["North's Rage", "Yukimura Sanada", "Isabella I"] },
+  
+  { heroes: ["Peace Bringer", "Genghis Khan", "Cao Cao"] },
   { heroes: ["Demon Spear", "Jiguang Qi", "Genghis Khan"] },
   { heroes: ["Demon Spear", "Jiguang Qi", "North's Rage"] },
   { heroes: ["Demon Spear", "Queen Anne", "William Wallace"] }
