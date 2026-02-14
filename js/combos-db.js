f180c2840fe2d3c365cc1b1ed9a924432cdb1657
aboroe1097-hash <aboroe1097@gmail.com>
2026-02-14 22:28:51 +0400

    Update combos-db.js

--- js/combos-db.js
+++ js/combos-db.js
@@ -2,7 +2,7 @@
 // Rank is determined by position in this array. First item = Rank 1.
 // Mapping order: [Front, Middle, Back]
   // Season 5 (X1)
- //  { name: 'Bewoulf',      
+ //  { name: 'Beowulf',      
  //  { name: 'Hunk',        
  //  { name: 'Boudica',      
  //  { name: 'Sakura',       
@@ -13,9 +13,13 @@
 
 export const rankedCombos = [
   // --- TIER 1: TOP GARRISON META (S4 Focus) ---
-  { heroes: ["Ramses II", "Leonidas", "Bewoulf"] },          // Rank 1
-  { heroes: ["Ramses II", "Cleopatra VII", "Bewoulf"] },      // Rank 3
-  { heroes: ["Bleeding Steed", "Ramses II", "Bewoulf"] },     // Rank 4
+  { heroes: ["Immortal Guardian", "Ramses II", "Beowulf"] },
+  { heroes: ["Boudica", "Jade Eagle", "Ramses II"] },
+  { heroes: ["Ramses II", "Leonidas", "Beowulf"] },          // Rank 1
+  { heroes: ["Ramses II", "Cleopatra VII", "Beowulf"] },      // Rank 3
+  { heroes: ["Bleeding Steed", "Ramses II", "Beowulf"] },     // Rank 4
+  { heroes: ["Boudica", "Sakura", "Jade Eagle"] },
+
   
   { heroes: ["Ramses II", "Leonidas", "Jade Eagle"] },          // Rank 1
   { heroes: ["Octavius", "The Brave", "Caesar"] },              // Rank 2
@@ -40,10 +44,30 @@ export const rankedCombos = [
  //  { name: 'Boudica',      
  //  { name: 'Sakura',  
   { heroes: ["Hunk", "Sakura", "Jade Eagle"] },      // Rank 3
-  { heroes: ["King Arthur", "Bleeding Steed", "Cao Cao"] },        
+  { heroes: ["Boudica", "Sakura", "ELK"] },
+  { heroes: ["Sakura", "ELK", "Jade Eagle"] },
+  { heroes: ["King Arthur", "Cicero", "Bleeding Steed"] },  // Control/Sustain
+  { heroes: ["King Arthur", "Bleeding Steed", "Cao Cao"] },     
+  { heroes: ["Sky Breaker", "Boudica", "Sakura"] },
+  { heroes: ["Boudica", "Cleopatra VII", "Al Fatih"] },
+  { heroes: ["Ramses II", "Immortal Guardian", "Witch Hunter"] },
+  { heroes: ["Boudica", "Al Fatih", "Ramses II"] },
+  { heroes: ["Ramses II", "Al Fatih", "Sakura"] },
   { heroes: ["Sky Breaker", "Ramses II", "Al Fatih"] },
+
+  { heroes: ["Hunk", "Cleopatra VII", "Cao Cao"] },        // Rapid Rage
+  { heroes: ["Hunk", "Bleeding Steed", "Cao Cao"] },       // Cav Speed
+  { heroes: ["Theodora", "Che Liu", "Cicero"] },            // Mixed Control
+  
   { heroes: ["Alfred", "Bleeding Steed", "Cleopatra VII"] },      
   { heroes: ["The Brave", "Bleeding Steed", "Jade Eagle"] }, 
+
+  { heroes: ["Immortal Guardian", "Sakura", "Jade Eagle"] },
+  { heroes: ["Charles the Great", "Ramses II", "Al Fatih"] },
+  { heroes: ["Sky Breaker", "Cleopatra VII", "Al Fatih"] },
+  { heroes: ["Boudica", "Inquisitor", "Witch Hunter"] },
+  { heroes: ["Sky Breaker", "Inquisitor", "Charles the Great"] },
+  
   { heroes: ["Wind-Walker", "Rozen Blade", "Immortal"] },  
   { heroes: ["Rozen Blade", "Cleopatra VII", "Immortal"] },     
   { heroes: ["BeastQueen", "Rozen Blade", "Immortal"] },     
@@ -54,7 +78,11 @@ export const rankedCombos = [
   { heroes: ["Alfred", "Jeanne d'Arc", "Lionheart"] },          // Rank 20
   { heroes: ["Charles the Great", "Ramses II", "Al Fatih"] },    
   { heroes: ["Theodora", "The Brave", "Jade Eagle"] },   
+
   
+  { heroes: ["Immortal Guardian", "Charles the Great", "Jade Eagle"] },
+  { heroes: ["Sky Breaker", "Inquisitor", "Sakura"] },
+  { heroes: ["Sakura", "Charles the Great", "Al Fatih"] },
   { heroes: ["War Lord", "Alfred", "Lionheart"] },              
   { heroes: ["Alfred", "Black Prince", "Lionheart"] },          
   { heroes: ["Alfred", "Black Prince", "Jeanne d'Arc"] },       
@@ -75,6 +103,9 @@ export const rankedCombos = [
   { heroes: ["Immortal Guardian", "Charles the Great", "Al Fatih"] },
   { heroes: ["Immortal Guardian", "Divine Arrow", "Al Fatih"] },
   { heroes: ["Immortal Guardian", "Divine Arrow", "Sky Breaker"] },
+
+  { heroes: ["Che Liu", "Cicero", "Desert Storm"] },        // Frontline Burst
+  { heroes: ["Che Liu", "Jade", "Cicero"] },                // Evasion/Control
   
   { heroes: ["War Lord", "Jeanne d'Arc", "Jane"] },               
   { heroes: ["Black Prince", "Cleopatra VII", "Jane"] },        
