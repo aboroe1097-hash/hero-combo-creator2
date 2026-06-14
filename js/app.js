ea96cf1b5b10bdf39720cc93b1d7a0eb74dca909
aboroe1097-hash <aboroe1097@gmail.com>
2026-06-14 04:11:51 +0400

    UI tweaks: Make doors full vertical height on mobile and disable audio

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ import { allHeroesData } from './heroes-data.js';
 import { heroBonusPoints } from './hero-bonuses.js';
 import { applySeo } from './seo.js';
 import { renderTechNodeIconSvg, resolveTechNodeIcon } from './research-node-icons.js';
-import { initAppLoading, notifyAppReady } from './app-loading.js';
+import { initAppLoading, notifyAppReady } from './app-loading.js?v=20260614_3';
 import { registerServiceWorker, setupInstallPrompt } from './pwa-register.js';
 import { loadPlayerProfileFromCloud, applyRosterToGenerator } from './player-profile.js';
 import { parseComboShareUrl } from './combo-share.js';
