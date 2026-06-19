c5d273f9e27f490dc6d7b67729e0de92041c1931
aboroe1097-hash <aboroe1097@gmail.com>
2026-06-20 03:47:27 +0400

    v10.5.2: fix catch-up hero icon crops

--- js/heroes-data.js
+++ js/heroes-data.js
@@ -67,17 +67,17 @@ export const allHeroesData = [
   { name: 'Spectral Reaper', season: 'X2', Type:'Archers', State:'Free', imageUrl: 'https://i.ibb.co/ZzRqFXCH/image-2026-06-09-133028179.png' },
   { name: 'Valkyrie',        season: 'X2', Type:'Footmen', State:'Free', imageUrl: 'https://i.ibb.co/8D8WG7My/image-2026-06-09-133104902.png' },
 
-  { name: "Ragnar the Demon's Lord", season: 'X1', Type:'All', State:'Free', releaseSeason:'X1', imageUrl: 'images/heroes/catchup/ragnar-demon-lord.png' },
+  { name: "Ragnar the Demon's Lord", season: 'X1', Type:'All', State:'Free', releaseSeason:'X1', imageUrl: 'images/heroes/catchup/ragnar-demon-lord.png?v=20260620_10' },
 
-  { name: 'Bjorn',             season: 'X8', Type:'All', State:'Free', releaseSeason:'X4', imageUrl: 'images/heroes/catchup/bjorn.png' },
-  { name: 'Skanda',            season: 'X8', Type:'All', State:'Free', releaseSeason:'X4', imageUrl: 'images/heroes/catchup/skanda.png' },
-  { name: 'Liberator',         season: 'X8', Type:'All', State:'Free', releaseSeason:'X5', imageUrl: 'images/heroes/catchup/liberator.png' },
-  { name: 'Ashen Verdict',     season: 'X8', Type:'All', State:'Free', releaseSeason:'X5', imageUrl: 'images/heroes/catchup/ashen-verdict.png' },
-  { name: 'Warden',            season: 'X8', Type:'All', State:'Free', releaseSeason:'X6', imageUrl: 'images/heroes/catchup/warden.png' },
-  { name: 'Warhammer',         season: 'X8', Type:'All', State:'Free', releaseSeason:'X6', imageUrl: 'images/heroes/catchup/warhammer.png' },
-  { name: 'Eidolon',           season: 'X8', Type:'All', State:'Free', releaseSeason:'X7', imageUrl: 'images/heroes/catchup/eidolon.png' },
-  { name: 'Scarlet Reaver',    season: 'X8', Type:'All', State:'Free', releaseSeason:'X7', imageUrl: 'images/heroes/catchup/scarlet-reaver.png' },
-  { name: 'Rainforest Ranger', season: 'X8', Type:'All', State:'Free', releaseSeason:'X8', imageUrl: 'images/heroes/catchup/rainforest-ranger.png' },
-  { name: 'Fortuneteller',     season: 'X8', Type:'All', State:'Free', releaseSeason:'X8', imageUrl: 'images/heroes/catchup/fortuneteller.png' },
-  { name: 'Cyrus',             season: 'X8', Type:'Cavalry', State:'Paid', releaseSeason:'SP', imageUrl: 'images/heroes/catchup/cyrus.png' }
+  { name: 'Bjorn',             season: 'X8', Type:'All', State:'Free', releaseSeason:'X4', imageUrl: 'images/heroes/catchup/bjorn.png?v=20260620_10' },
+  { name: 'Skanda',            season: 'X8', Type:'All', State:'Free', releaseSeason:'X4', imageUrl: 'images/heroes/catchup/skanda.png?v=20260620_10' },
+  { name: 'Liberator',         season: 'X8', Type:'All', State:'Free', releaseSeason:'X5', imageUrl: 'images/heroes/catchup/liberator.png?v=20260620_10' },
+  { name: 'Ashen Verdict',     season: 'X8', Type:'All', State:'Free', releaseSeason:'X5', imageUrl: 'images/heroes/catchup/ashen-verdict.png?v=20260620_10' },
+  { name: 'Warden',            season: 'X8', Type:'All', State:'Free', releaseSeason:'X6', imageUrl: 'images/heroes/catchup/warden.png?v=20260620_10' },
+  { name: 'Warhammer',         season: 'X8', Type:'All', State:'Free', releaseSeason:'X6', imageUrl: 'images/heroes/catchup/warhammer.png?v=20260620_10' },
+  { name: 'Eidolon',           season: 'X8', Type:'All', State:'Free', releaseSeason:'X7', imageUrl: 'images/heroes/catchup/eidolon.png?v=20260620_10' },
+  { name: 'Scarlet Reaver',    season: 'X8', Type:'All', State:'Free', releaseSeason:'X7', imageUrl: 'images/heroes/catchup/scarlet-reaver.png?v=20260620_10' },
+  { name: 'Rainforest Ranger', season: 'X8', Type:'All', State:'Free', releaseSeason:'X8', imageUrl: 'images/heroes/catchup/rainforest-ranger.png?v=20260620_10' },
+  { name: 'Fortuneteller',     season: 'X8', Type:'All', State:'Free', releaseSeason:'X8', imageUrl: 'images/heroes/catchup/fortuneteller.png?v=20260620_10' },
+  { name: 'Cyrus',             season: 'X8', Type:'Cavalry', State:'Paid', releaseSeason:'SP', imageUrl: 'images/heroes/catchup/cyrus.png?v=20260620_10' }
 ];
