99c956dbfc78500fa02217d576b795f798b5a0ee
aboroe1097-hash <aboroe1097@gmail.com>
2026-07-05 20:15:42 +0400

    Constrain admin attack history height

--- js/admin-page.js
+++ js/admin-page.js
@@ -103,7 +103,7 @@ async function bootAdminPage() {
     await loadTranslationsForLanguage(nextLang);
     updateTextContent(nextLang);
   });
-  const mod = await import('./ocr-dashboard.js?v=20260705_144535');
+  const mod = await import('./ocr-dashboard.js?v=20260705_161239');
   await mod.bootOcrDashboard();
 }
 
